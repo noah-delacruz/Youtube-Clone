9b9ce67e0b0ab221cf496c13744caece1ddb423b
Noah dela Cruz <delacruznoah12898@yahoo.com>
2023-07-30 01:30:06 -0700

    Add google sign in function with firebase

---
+++ yt-web-client/app/firebase/firebase.ts
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, User } from "firebase/auth";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCylHgNvVFbXV0fK8OWlHIEdEYFaNOZl-Y",
+  authDomain: "yt-clone-1dd65.firebaseapp.com",
+  projectId: "yt-clone-1dd65",
+  appId: "1:55192917044:web:776a9659281d99945e6661",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+/*
+ * Signs the user in with a Google popup
+ * @returns A promise that resolves with the user's credentials
+ */
+export function signInWithGoogle() {
+    return signInWithPopup(auth, new GoogleAuthProvider());
+}
