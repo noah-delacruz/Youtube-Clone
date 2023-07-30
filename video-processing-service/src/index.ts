74e0de24267cc541102496b6c7609d4e0d7ac507
Noah dela Cruz <delacruznoah12898@yahoo.com>
2023-07-29 22:26:14 -0700

    Setup next app to create front-end

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,16 +1,18 @@
 import express from "express";
 import { convertVideo, deleteProcessedVideo, deleteRawVideo, downloadRawVideo, setupDirectories, uploadProcessedVideo } from "./storage";
 
+// Create local directories for videos
 setupDirectories();
 
 const app = express();
 app.use(express.json());
 
+// Process a video file from Cloud Storage into 360p
 app.post("/process-video", async (req, res) => {
     // Get the bucket and filename from the Cloud Pub/Sub message
     let data;
     try {
-        const message = Buffer.from(req.body.message, 'base64').toString('utf8');
+        const message = Buffer.from(req.body.message.data, 'base64').toString('utf8');
         data = JSON.parse(message);
         if (!data.name) {
             throw new Error('Invalid message payload received');
