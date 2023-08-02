02b79d2e2382c8ee8f0d3af6fa153d44082fb6b1
Noah dela Cruz <delacruznoah12898@yahoo.com>
2023-08-02 04:22:57 -0700

    Add cloud function to get videos from Firestore database

--- yt-api-service/functions/src/index.ts
+++ yt-api-service/functions/src/index.ts
@@ -16,6 +16,17 @@ const storage = new Storage();
 // Bucket for users to upload to | App downloads from here
 const rawVideoBucketName = "nd12898-yt-raw-videos";
 
+const videoCollectionId = "videos";
+
+export interface Video {
+    id?: string,
+    uid?: string,
+    filename?: string,
+    status?: "processing" | "processed",
+    title?: string,
+    description?: string
+}
+
 export const createUser = functions.auth.user().onCreate((user) => {
   const userInfo = {
     uid: user.uid,
@@ -51,4 +62,10 @@ export const generateUploadUrl = onCall({maxInstances: 1}, async (request) => {
   });
 
   return {url, fileName};
+});
+
+export const getVideos = onCall({maxInstances: 1}, async () => {
+  const snapshot =
+    await firestore.collection(videoCollectionId).limit(10).get();
+  return snapshot.docs.map((doc) => doc.data());
 });
