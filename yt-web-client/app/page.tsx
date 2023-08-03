64f2bfc4d46200bfa49610cffa98eed06cb59723
Noah dela Cruz <delacruznoah12898@yahoo.com>
2023-08-02 17:33:44 -0700

    Display videos on homepage

--- yt-web-client/app/page.tsx
+++ yt-web-client/app/page.tsx
@@ -1,15 +1,20 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from 'next/image';
+import styles from './page.module.css';
+import { getVideos } from './firebase/functions';
+import Link from 'next/link';
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-      </div>
-    </main>
+export default async function Home() {
+    const videos = await getVideos();
+
+    return (
+        <main>
+            {
+                videos.map((video) => (
+                    <Link href={`/watch?v=${video.filename}`}>
+                        <Image src={'/thumbnail.png'} alt='video' width={120} height={80} className={styles.thumbnail} />
+                    </Link>
+                ))
+            }
+        </main>
   )
 }
