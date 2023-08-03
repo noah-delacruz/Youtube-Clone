401d7e35c73dc7d75fe66f39a19c3dec4d0a3add
Noah dela Cruz <delacruznoah12898@yahoo.com>
2023-08-02 17:51:45 -0700

    Render video on watch page

--- yt-web-client/app/watch/page.tsx
+++ yt-web-client/app/watch/page.tsx
@@ -1,7 +1,14 @@
+'use client';
+
+import { useSearchParams } from 'next/navigation';
+
 export default function Watch() {
+    const videoPrefix = 'https://storage.googleapis.com/nd12898-yt-processed-videos/';
+    const videoSrc = useSearchParams().get('v');
     return (
         <div>
             <h1>Watch page</h1>
+            <video controls src={videoPrefix + videoSrc}/>
         </div>
     );
 }
